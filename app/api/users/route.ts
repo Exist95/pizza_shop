7ecb3a4bd3a284ccba79c636abad6817a61f2701
Exist95 <jyeol0210@naver.com>
2025-02-25 10:28:33 +0900

    prisma schema 업데이트

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/prisma/prisma-client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   const users = await prisma.user.findMany();
@@ -7,6 +7,12 @@ export async function GET() {
   return NextResponse.json(users);
 }
 
-export function POST() {
+export async function POST(req: NextRequest) {
+  const data = await req.json();
 
+  const user = await prisma.user.create({
+    data: data
+  })
+
+  return NextResponse.json(user);
 }
